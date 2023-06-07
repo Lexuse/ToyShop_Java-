e80abb757b58ee71ede51a39ed3e97aaafb14455
Lexuse <Lexuse2007@gmail.com>
2023-06-07 16:43:41 +0300

    Add some changes

--- Program.java
+++ Program.java
@@ -1,13 +1,19 @@
 import model.goods.toy.ToyCreate;
 import model.goods.toy.ToysStore;
 import model.goods.toy.TypeOfToy;
+import model.warehous.WarehouseType;
+import model.warehous.Warehouses;
 
 public class Program {
     public static void main (String[] args){
-        ToyCreate toyCreater;
-        ToysStore toystore = new ToysStore();
-        toyCreater = new ToyCreate(TypeOfToy.Кукла, "Барби", 60, 3);
-        //System.out.println(toystore.toString());
+//        ToyCreate toyCreater;
+//        ToysStore toystore = new ToysStore();
+//        toyCreater = new ToyCreate(TypeOfToy.Кукла, "Барби", 60, 3);
+//        //System.out.println(toystore.toString());
+        Warehouses war = new Warehouses();
+        war.addWarehouse("Склад 1", WarehouseType.Книги);
+        war.addWarehouse("Склад 2", WarehouseType.Игрушки);
 
+        System.out.println(war.warehousesInfo());
     }
 }
--- model/warehous/Warehouses.java
+++ model/warehous/Warehouses.java
@@ -5,18 +5,21 @@ import java.util.List;
 
 public class Warehouses {
     private List<Warehouse> warehouses = new ArrayList<>();
+    Warehouse warehouse;
 
     public void addWarehouse (String name, WarehouseType type){
-        warehouses.add (new Warehouse(name, type));
+        warehouse = new Warehouse(name, type);
+        warehouses.add (warehouse);
+
     }
 
-    public String warehousesInfo(Warehouse warehouse){
+    public String warehousesInfo(){
         StringBuilder result = new StringBuilder();
-        Warehouse Warehouse = new Warehouse(warehouse.getName(), warehouse.getType());
+        result.append("Количество имеющихся складов: " + warehouses.size() + "\n");
         for (Warehouse war : warehouses){
-            result.append(Warehouse.getName()).append("/n");
-            result.append(Warehouse.getType()).append("/n");
-            result.append(Warehouse.getiD()).append("/n");
+            result.append("Название " + warehouses.indexOf(warehouse) + "-го " + "склада "  + ": " + warehouse.getName()+ "\n");
+            result.append("Тип " + warehouses.indexOf(warehouse)+ "-го " +  "склада " + ": " + warehouse.getType() + "\n");
+            result.append("ИД " + warehouses.indexOf(warehouse)+ "-го " + "склада " + ": " + warehouse.getiD() + "\n" + "\n");
         }
         return result.toString();
     }
